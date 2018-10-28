3dc3e63c1aa829d52f7017af5f17cf855f020048
Android_Arsh <android_arsh@Arshs-MacBook-Pro.local>
2018-10-28 16:26:38 -0700

    added methods for determing hypotenuse distance and knowing which ghost is outside center box, and updated pacman movement strategy

--- js/agent.js
+++ js/agent.js
@@ -128,10 +128,144 @@ function drawDebug() {
   }
 }
 
+
+
+//assign a number to each ghost for easy reference.
+var B_NUM = 1; //blinky
+var P_NUM = 2; //pinky
+var I_NUM = 3; //inky
+var C_NUM = 4; //clyde
+// when the ghost is in the center storage area, it's coordinate values will be as such:
+var minXstorage = 238;
+var maxXstorage = 314;
+var yStorage = 258;
+
+
+
+
+//returns a list of ghosts #'s who are outside the center storage area (NOT THE TUNNEL!)
+function checkOutside() {
+  var list = [];
+  
+  if (checkOutsideHelper(GHOST_BLINKY_POSITION_X, GHOST_BLINKY_POSITION_Y)) {list.push(0)};
+  if (checkOutsideHelper(GHOST_PINKY_POSITION_X, GHOST_PINKY_POSITION_Y)) {list.push(1)};
+  if (checkOutsideHelper(GHOST_INKY_POSITION_X, GHOST_INKY_POSITION_Y)) {list.push(2)};
+  if (checkOutsideHelper(GHOST_CLYDE_POSITION_X, GHOST_CLYDE_POSITION_Y)) {list.push(3)};
+
+  console.info("number of ghosts who are outside: " + list.length );
+  
+  return list;
+}
+//helper function for checking if the ghost is outside the center storage
+function checkOutsideHelper(theX, theY) {
+  if (theX < minXstorage || theX > maxXstorage || theY != yStorage) {
+    return true; //the ghost is outside the center storage
+  } else{
+    return false; //the ghost is not outside center storage
+  }
+}
+
+//returns a list of distances of each ghost
+function ghostDistances(){
+  var distList = [];
+  distList.push(distanceToPacman(GHOST_BLINKY_POSITION_X, GHOST_BLINKY_POSITION_Y));
+  distList.push(distanceToPacman(GHOST_PINKY_POSITION_X, GHOST_PINKY_POSITION_Y));
+  distList.push(distanceToPacman(GHOST_INKY_POSITION_X, GHOST_INKY_POSITION_Y));
+  distList.push(distanceToPacman(GHOST_CLYDE_POSITION_X, GHOST_CLYDE_POSITION_Y));
+  return distList;
+}
+
+//returns hypotenuse distance
+function distanceToPacman(ghostX, ghostY) {
+  var a = PACMAN_POSITION_X - ghostX;
+  var b = PACMAN_POSITION_Y - ghostY;
+  var c = Math.sqrt( (a*a) + (b*b) );
+  return c;
+}
+
+//finds the nearest ghost out of the list of ghosts that are not in the tunnel.
+function findNearestGhost(outList, distList) {
+  var lowestDistance = 100000;
+  var lowestGhost; //number corresponding to which ghost is closest
+  for (i = 0; i < outList.length; i++) {
+    var dist = distList[outList[i]];
+    //logger.info("distance of " + i + " ghost is " + dist);
+    if (lowestDistance > dist) {
+      lowestDistance = dist;
+      lowestGhost = outList[i];
+    }
+  }
+  return lowestGhost;
+}
+
+//pass in the number correspending to the ghost that should be copied.
+function copyGhostDirec(ghostNum) {
+  if (ghostNum = 0) {
+    return GHOST_BLINKY_DIRECTION;
+  } else if (ghostNum = 1) {
+    return GHOST_PINKY_DIRECTION;
+  } else if (ghostNum = 2) {
+    return GHOST_INKY_DIRECTION;
+  } else {
+    return GHOST_CLYDE_DIRECTION;
+  }
+}
+
+
 function selectMove() {
 
     buildGameboard();
 
+    var outsideTunnelList = checkOutside();   //check the position of each ghost. if they are not in the box. 
+    var distanceList = ghostDistances();  //find the distance of each ghost from pacman
+    var nearGhost = findNearestGhost(outsideTunnelList, distanceList); //variable for nearest ghost
+    
+    console.info("nearest ghost is " + nearGhost);
+
+    if (!PACMAN_DEAD && !GAMEOVER) { // make sure the game is running
+
+      var directions = [];
+      for (var i = 1; i < 5; i++) {
+          if (canMovePacman(i)) directions.push(i);
+      }
+
+      //do the same thing that the nearest ghost is doing.
+      if (directions.length > 2 || !PACMAN_MOVING) {
+        //if pacman can move in the direction that the nearest ghost is heading
+        if (canMovePacman(copyGhostDirec(nearGhost))) {
+          movePacman(copyGhostDirec(nearGhost)); //then move him in that direction
+        } else { //else move him in a random direction //later on make sure he doesnt move in the direction of the ghost
+          movePacman(directions[Math.floor(Math.random() * directions.length)]);
+        }
+      }
+  }
+ 
+
+  //TODO: 
+  
+  //make pacman change direction while moving rather than continuing his current direction 
+  //if nearest ghost is within distance of 4, go in opposite direction of that ghost (dont do same thing, maybe ghost will turn away and you can create distance)
+  //? find the path distance of each ghost from pacman (the path which the ghost would have to take to reach pacman) ?
+
+};
+
+
+
+
+
+
+
+setInterval(drawDebug, 1000/3);
+
+
+
+
+
+
+
+
+
+    /* //Old code below 
     if (!PACMAN_DEAD && !GAMEOVER) { // make sure the game is running
 
         var directions = [];
@@ -141,6 +275,6 @@ function selectMove() {
 
         if (directions.length > 2 || !PACMAN_MOVING) movePacman(directions[Math.floor(Math.random() * directions.length)]);
     }
-};
+    */
 
-setInterval(drawDebug, 1000/3);
+
